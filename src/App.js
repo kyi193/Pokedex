a337afd2cf0b4068353a96c38e0ba7ee79b7e35e
Kevin <kevin.yi93@gmail.com>
2020-04-07 13:25:07 -0400

    v1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {HashRouter as Router, Route, Switch} from 'react-router-dom'
 
-import "bootstrap/dist//css/bootstrap.min.css";
+
 
 import './App.css';
 import Navbar from "./components/layout/Navbar";
